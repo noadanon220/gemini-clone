7a4355f99ff99673ec46e0310f87eacf1bf2c075
noadanon220 <35223664+noadanon220@users.noreply.github.com>
2025-11-23 13:42:52 +0200

    Enhance UI and add new chat functionality

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -11,7 +11,7 @@ async function runChat(prompt) {
 
         const genAI = new GoogleGenerativeAI(apiKey);
         const model = genAI.getGenerativeModel({
-            model: "gemini-1.5-flash-latest"
+            model: "gemini-1.5-flash"
         });
 
         const result = await model.generateContent(prompt);
